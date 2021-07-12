45fe29eedbd3c8834c1baf49b98d5179503ab17f
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-07-12 22:10:14 +0700

    add: profile picture on about page

---
+++ components/Pfp.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-img :src="avatar.src">
+        <template v-slot:placeholder>
+            <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center">
+                <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5">
+                </v-progress-circular>
+            </v-row>
+        </template>
+    </v-img>
+</template>
+
+<script>
+export default {
+    name: 'Pfp',
+    data() {
+        return {
+            avatar: {
+                src: require("~/assets/pfp.jpg")
+        }
+        }
+    }
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <h1 class="v-heading text-center text-h4 text-sm-h2 text-center mb-12">About me</h1>
+        <v-row justify="center">
+            <v-avatar
+            color="grey lighten-2"
+            size="256"
+            >
+                <Pfp />
+            </v-avatar>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import Pfp from '~/components/Pfp.vue';
+
+export default {
+    head: {
+        title: 'About',
+    },
+    components: {
+        Pfp
+    }
+}
+</script>
