69c33181e94dcefc196529eaa43694b77eadc5d3
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-07-13 11:46:06 +0700

    add: artworks page

---
+++ pages/artwork.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <img src="~/assets/banner.png" width="610" height="180">
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+export default {
+    head: {
+        title: 'Artwork',
+    },
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
