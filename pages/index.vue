e9f0d31139fa49d2a4cd7e58337b2bd75ad14214
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-07-14 19:59:19 +0700

    add: subtitle

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
-    <h1 class="v-heading text-center text-h4 text-sm-h2 text-center mb-8">Kitpipat Jaritwong</h1>
+    <h1 class="v-heading text-center text-h4 text-sm-h2 text-center mb-4">Kitpipat Jaritwong</h1>
+    <h4 class="font-weight-light text-h8 text-sm-h6 text-center mb-8">Student, Frontend Developer, Designer</h4>
     <v-row justify="center">
       <v-btn target="_blank" text large v-for="(item, i) in items" 
       :key="i" :href="item.to">{{ item.title }}</v-btn>
