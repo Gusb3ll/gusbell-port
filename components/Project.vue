f4e1f1aa2ac789fedea3a0f9f939cc0bc315b81c
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-07-17 17:46:44 +0700

    add: projects page

---
+++ components/Project.vue
@@ -0,0 +1,76 @@
+<template>
+    <v-container>
+        <div class="block GalleryBlock">
+            <v-row>
+                <v-col
+                v-for="project in projects"
+                :key="project.id"
+                class="d-flex child-flex"
+                cols="4">
+                    <v-card elevation="2" outlined shaped dark style="background-color: rgba(0, 0, 0, 0.2)">
+                        <v-card-title> {{ project.name }} </v-card-title>
+                        <v-card-subtitle> {{ project.sub }} </v-card-subtitle>
+                        <v-card-text> {{ project.desc }} </v-card-text>
+                        <v-card-actions>
+                            <v-btn v-if="project.to" style="background-color: transparent;" :href="project.to">
+                                <v-icon>mdi-github</v-icon>
+                            </v-btn>
+                            <v-btn v-if="project.to2" style="background-color: transparent;" :href="project.to2">
+                                <v-icon>mdi-earth</v-icon>
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>      
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Project',
+    data() {
+        return {
+            projects : [
+                {
+                    id: 1,
+                    name: 'oturna (Private)',
+                    sub: 'Frontend/Backend',
+                    desc: 'A tournament manager for rhythm game, osu!', 
+                    to: '',
+                    to2: 'https://omthpl.varkaria.tech'
+                },
+                {
+                    id: 2,
+                    name: 'guweb',
+                    sub: 'Frontend/Backend',
+                    desc: 'Front-facing appearance of the osu! server protocol',
+                    to: 'https://github.com/Varkaria/guweb'
+                },
+                {
+                    id: 3,
+                    name: 'Holotranslator',
+                    sub: 'Author',
+                    desc: 'Live Japanese speech to text translator.',
+                    to: 'https://github.com/Gusb3ll/Holotranslator'
+                },
+                {
+                    id: 4,
+                    name: 'Euler-bot',
+                    sub: 'Author',
+                    desc: 'A bot for mathmetic POSN discord.',
+                    to: 'https://github.com/Gusb3ll/Euler-bot'
+                },
+                {
+                    id: 5,
+                    name: 'gusbell-port',
+                    sub: 'Author',
+                    desc: 'This portfolio that are written in Nuxt.js',
+                    to: 'https://github.com/Gusb3ll/gusbell-port',
+                    to2: 'https://gusbell-port.vercel.app'
+                }
+            ]
+        }
+    }
+}
+</script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,12 +1,20 @@
 <template>
-
+    <v-container>
+        <h1 class="v-heading text-center text-h4 text-sm-h2 mb-12">My Projects</h1>
+        <Project />
+    </v-container>
 </template>
 
 <script lang="ts">
+import Project from '~/components/Project.vue'
+
 export default {
     layout: 'default',
     head: {
         title: 'Projects'
     },
+    components: {
+        Project
+    }
 }
 </script>
