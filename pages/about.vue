c9df1b7d212118984ec065b631aaa7a91ca96293
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-08-11 18:32:04 +0700

    [o] Fix margin

--- pages/about.vue
+++ pages/about.vue
@@ -10,8 +10,8 @@
         </v-row>
         <v-main class="d-flex wrap">
             <h1 class="text-center text-h5 text-sm-h3 pb-8">Hi, I am Kitpipat Jaritwong</h1>
-            <v-card outlined dark class="px-4 py-4" style="background-color: transparent">
-                <p class="text-center text-sm-h5 text-h7">I am 17 years old, and I am currently studying at Thammasat KhlongLuang Withayakhom School. <br><br> 
+            <v-card outlined dark class="px-16 py-4 mx-16" style="background-color: transparent">
+                <p class="mx-16 text-center text-sm-h5 text-h7">I am 17 years old, and I am currently studying at Thammasat KhlongLuang Withayakhom School. <br><br> 
                 I am very interested in coding and designing, for the coding part I mainly does the frontend part and sometimes does the backend part, for the
                 designing part, I love designing 'futuristic' looking design as shown on my artwork page. <br><br>
                 I am also interested in japanese culture and their design, most of my design are mostly reference from japanese design. I also love their manga, 
